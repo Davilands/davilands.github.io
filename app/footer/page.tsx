430ccd9f77823fc4e3fafdb52dc6fa176eabb125
theanishtar <dangtt135@gmail.com>
2024-10-15 12:34:40 +0700

    style: basic style footer

--- app/footer/page.tsx
+++ app/footer/page.tsx
@@ -6,26 +6,10 @@ export default function Footer() {
   return (
     <div>
       <footer>
-        <div className="footerContainer">
-          <div className="socialIcons">
-            <a href=""><i className='bx bxl-youtube'></i></a>
-            <a href=""><i className='bx bxl-tiktok' ></i></a>
-            <a href=""><i className='bx bxl-github' ></i></a>
-            <a href=""><i className='bx bxl-facebook' ></i></a>
-            {/* <a href=""><i className="fa-brands fa-youtube"></i></a> */}
-          </div>
-          <div className="footerNav">
-            <ul><li><a href="">Home</a></li>
-              <li><a href="">News</a></li>
-              <li><a href="">About</a></li>
-              <li><a href="">Contact Us</a></li>
-              <li><a href="">our Team</a></li>
-            </ul>
-          </div>
+        <div className="footer">
+          <p>Released under the MIT License.</p>
 
-        </div>
-        <div className="footerBottom">
-          <p>Copyright &copy;2023; Designed by <span className="designer">Noman</span></p>
+          <p>Copyright © 2024 Davis</p>
         </div>
       </footer>
     </div>
